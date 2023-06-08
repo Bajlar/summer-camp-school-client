d2bd39eabdbc9b9f97e65a8a3ee171347ac079b5
Bajlar01~ <bajlarrahman24@gmail.com>
2023-06-08 11:21:04 +0600

    footer is  done

--- documentaction.js
+++ documentaction.js
@@ -2,53 +2,44 @@
 
 // ## **Theme: Summer Camp Learning School** :school:
 
+You are asked to build a website for a summer camp school that allows students to enroll and learn specific extra-curricular activities discussed in the requirements. The website must be exclusively dedicated to one type of school **(Music School, Music Instrument Learning School, Dance Learning School, Art & craft School, Foreign Language Learning School, Drama/Theatre school, Sports academies, Drawing School, Design school, Photography School, Culinary School, Magic/Illusion School, Martial Art, Fashion Design, Film School, News Presenter School, Radio Jockey, weight loss school, Yoga and meditation school, Makeup and cosmetology School, etc.)**. Your website will be based on one of the categories mentioned above. If you choose, for example, the category photography school, your website should have all the information and activities based on photography only. 
 
-/* You are asked to build a website for a summer camp school that allows students to enroll and learn specific extra-curricular activities discussed in the requirements. The website must be exclusively dedicated to one type of school **(Music School, Music Instrument Learning School, Dance Learning School, Art & craft School, Foreign Language Learning School, Drama/Theatre school, Sports academies, Drawing School, Design school, Photography School, Culinary School, Magic/Illusion School, Martial Art, Fashion Design, Film School, News Presenter School, Radio Jockey, weight loss school, Yoga and meditation school, Makeup and cosmetology School, etc.)**. Your website will be based on one of the categories mentioned above. If you choose, for example, the category photography school, your website should have all the information and activities based on photography only. 
+// > **Note: You can not do anything related to coding School, like web development, app development, robotics, etc. programming related school.**
 
-> **Note: You can not do anything related to coding School, like web development, app development, robotics, etc. programming related school.**
+// Make sure your website design is :unicorn: unique. Remember, a :unicorn: the unique project will add more value to your portfolio. :v:
 
-Make sure your website design is :unicorn: unique. Remember, a :unicorn: the unique project will add more value to your portfolio. :v:
+//    - Visit ThemeForest, Dribble, google, etc., for ideas.
+//    - Visit [this blog to get free images, illustrations & animations for your website.](https://bootcamp.uxdesign.cc/free-images-and-resources-collection-for-website-c77f2fc46ce5)
+//    - Try to explore component libraries other than DaisyUI. You can explore MUI, Ant design, Chakra UI etc.
+// <hr/>
+// Read the tasks carefully.
 
-   - Visit ThemeForest, Dribble, google, etc., for ideas. 
-   - Visit [this blog to get free images, illustrations & animations for your website.](https://bootcamp.uxdesign.cc/free-images-and-resources-collection-for-website-c77f2fc46ce5)
-   - Try to explore component libraries other than DaisyUI. You can explore MUI, Ant design, Chakra UI etc.
-<hr/>
-Read the tasks carefully.
-<hr/>
-<br/>
+// ## :writing_hand: **Main Requirements**
 
-## :writing_hand: **Main Requirements**
+// 1. Focus on making the website visually appealing. Ensure that
+//     - color contrast is pleasing to the eye
+//     - the website does not have a **"gobindo design"**
+//     - the website has proper alignment and space
+//     - Do not directly copy-paste a component design from a component library you are using. You will have to customize the design. (e.g. If you are using  DaisyUi & you decided to use a card component, you will have to customize the styling of that card rather than just copy & paste it)
 
-1. Focus on making the website visually appealing. Ensure that
-    - color contrast is pleasing to the eye
-    - the website does not have a **"gobindo design"**
-    - the website has proper alignment and space
-    - Do not directly copy-paste a component design from a component library you are using. You will have to customize the design. (e.g. If you are using  DaisyUi & you decided to use a card component, you will have to customize the styling of that card rather than just copy & paste it)
+    // >**Note:** Your website must not be related to your previous assignments' layout/ design or any practice project shown in the course modules or our conceptual sessions. Ex: You can't copy any design or similar functionality/ layout of
 
-    <br/>
-    
-    >**Note:** Your website must not be related to your previous assignments' layout/ design or any practice project shown in the course modules or our conceptual sessions. Ex: You can't copy any design or similar functionality/ layout of
+    //     ● Dragon news
+    //     ● Espresso Emporium
+    //     ● Car Doctor
+    //     ● Travel guru
+    //     ● Volunteer network
+    //     ● Bistro Boss
+    //     ● Doc House
+    //     ● Job Portal Website/ Aircnc or Any conceptual session project etc.
 
-        ● Dragon news
-        ● Espresso Emporium
-        ● Car Doctor
-        ● Travel guru
-        ● Volunteer network
-        ● Bistro Boss
-        ● Doc House
-        ● Job Portal Website/ Aircnc or Any conceptual session project etc.
-
-    **If any similarities are found, you will get zero(0) as a penalty.**
-
-    <br/>
+    // **If any similarities are found, you will get zero(0) as a penalty.**
 
 2. Make sure to keep the navbar and footer **on all the pages except on the 404 page**. Create a good, stylish, and meaningful footer. (including website logo or name or both, copyright, some contact information, Address etc.)
 
-<br/>
 
 3. Your website should have a navbar with the **Website logo or Website name, Home, Instructors,  Classes, Dashboard** and **User profile picture**\. The **user profile picture** and **Dashboard** on the navbar are conditional\. If the user is signed in, the navbar will show the profile picture; otherwise, it will show the **Login button**\.
 
-<br/>
 
 4. **Registration & Login System:**
 
@@ -58,8 +49,6 @@ Read the tasks carefully.
     - Password **(This field can hide/unhide the password by clicking on an icon)**
     - A link that will redirect to the registration page
     - Keep at least one social login
-
-    <br/>
 
     **Registration Page:**
     The Registration page will have the Email/Password form having the following fields:
@@ -72,13 +61,9 @@ Read the tasks carefully.
     - (optional) Phone Number
     - (optional) Address
 
-    <br/>
-
     >**Note:** Keep at least one social login
 
     >**Note:** Do not enforce **forget password feature** and  the **email verification method**, as it will inconvenience the examiner. If you want, you can add email verification/forget the password after receiving the assignment result.
-
-    <br/>
 
     **Error for Registration system:**
     On the Registration page, display errors when:
@@ -87,8 +72,6 @@ Read the tasks carefully.
     - is less than 6 characters
     - don't have a capital letter
     - don't have a special character
-
-    <br/>
 
     >**Note:** A user cannot submit empty email and password fields.
 
@@ -102,8 +85,6 @@ Read the tasks carefully.
     
     - **Extra Section -** Add one relevant sections. Make it attractive :heart_eyes:. Try to use animation effects.
 
-<br/>
-
 6. **Instructors Page:**
 Display all the instructors. Each Instructor will be:
    - Image
@@ -112,8 +93,6 @@ Display all the instructors. Each Instructor will be:
    - (optional) Number of Classes taken by the Instructor. 
    - (optional) Name of the Classes taken by the Instructor. 
    - (optional) `See Classes` button to show classes by this Instructor. This will take you to a new link
-<br/>
-<br/>
 
 7. **Classes page:**
 Show all approved classes on this page. Each Class will have the following:
@@ -126,11 +105,6 @@ Show all approved classes on this page. Each Class will have the following:
         - Available seats are 0
         - Logged in as admin/instructor
     - The class card background will be red if the available seats are 0.
-   <br/>
-
-
-<br/>
-
 
 8.  **Student Dashboard:**
     <br/>
@@ -140,19 +114,13 @@ Show all approved classes on this page. Each Class will have the following:
 
      - **My Enrolled Classes:**  Show all the Classes a student selected after successful payment.
 
-<br/>
-
 9. **Payment: (Student Dashboard)**
 
     - **Pay button Action:** Upon clicking the **Pay button** for a Class on the My Selected Classes, the student will be redirected to the payment page to finalize their payment. After a successful payment, the **Available seats** for the particular Class will be reduced by 1. The Class information will be shown on the **My Enrolled Classes page**and removed from the **My Selected Classes page**.
 
     - Create a payment history page for students. It will show the payment made by that student. Make sure to sort the payment history descending. The newest payment will be at the top
 
-
-<br/>
-
 10.  **Instructor Dashboard:**
-    <br/>
     ***It will be private & only an instructor can access this route.***
       
       - **Add a Class:** This page will have a form with the following fields:
@@ -174,9 +142,6 @@ Show all approved classes on this page. Each Class will have the following:
             - There will be no feedback if the Class is in pending or approved status.
             - If the Class is in the denied state by the admin, at that time, an admin can write feedback explaining why the Class was denied, which will appear in the feedback column.
 
-    
-<br/>
-
 11.  **Admin Dashboard:**
     <br/>
     ***It will be private & only an admin can access this route.***
@@ -189,7 +154,7 @@ Show all approved classes on this page. Each Class will have the following:
 
          >**Note:** A modal will be opened When the admin clicks the send feedback button. Admin will write the approved/denied reason in a text field and send it to the Instructor. You can also do this in another route except for the modal.
 
-        </br>
+        
 
        - **Manage Users:**  The admin can see the relevant information of all registered users. By default, everyone will be a student. And there will be **2 buttons**: **Make Instructor** button and **Make Admin** button. If a user clicks any of the buttons, disable it.
 
@@ -198,54 +163,40 @@ Show all approved classes on this page. Each Class will have the following:
            >**Note:** If the admin makes a user instructor, that user profile page will have the Instructor as the role
 
          - **Make Admin button Action:** Update the user role as an admin.
-          </br>
+          
 
 12. Use **relevant icons** in your **dashboard menus**, animations on your website.\. You have to use **anyone** of the animation libraries below:
     - [Framer-motion](https://www.framer.com/motion/)
     - [React-awesome-reveal](https://react-awesome-reveal.morello.dev/)
     - [React-spring](https://www.react-spring.dev/)
-    - [React-anime](https://github.com/juliangarnier/anime) */
+    - [React-anime](https://github.com/juliangarnier/anime)
 
-// <hr/>
-// <br/>
+## :writing_hand: **Bonus Task:**
 
-// ## :writing_hand: **Bonus Task:**
+1. Minimum 20 meaningful git commits on the client-side repository and 12 meaningful commits on the server-side repository. Feel free to commit as many as needed to save your progress during load-shedding.
 
-// 1. Minimum 20 meaningful git commits on the client-side repository and 12 meaningful commits on the server-side repository. Feel free to commit as many as needed to save your progress during load-shedding.
+2. Create a readme for the client-side and write about your project.
 
-// 2. Create a readme for the client-side and write about your project.
+    - Website name or logo or both
+    - Minimum 5 points of your website features
+    - Used packages/ technology name
+    - Live site link
 
-//     - Website name or logo or both
-//     - Minimum 5 points of your website features
-//     - Used packages/ technology name
-//     - Live site link
+3. Implementing a dark/light theme toggle for the home page. It's optional to implement the theme toggle for the entire website\.
 
-// 3. Implementing a dark/light theme toggle for the home page. It's optional to implement the theme toggle for the entire website\.
+4. Make the home page responsive. If possible, Make your entire website responsive\. (It is ok if the table is not responsive\.)
 
-// 4. Make the home page responsive. If possible, Make your entire website responsive\. (It is ok if the table is not responsive\.) 
+5. Use tanstack query (react-query) in at least two places. If needed, you can use it everywhere.
 
-// 5. Use tanstack query (react-query) in at least two places. If needed, you can use it everywhere. 
+6. use Axios for at least two calls.
 
-// 6. use Axios for at least two calls.
+7.  Must Use [react-hook-form](https://react-hook-form.com/) in the registration & login page. Using react hook form in another place is optional.
 
-// 7.  Must Use [react-hook-form](https://react-hook-form.com/) in the registration & login page. Using react hook form in another place is optional.
+8. Use the Environment variables both on the client side & server side\.
 
-// </br>
-
-// 8. Use the Environment variables both on the client side & server side\.
-
-// </br>
-
-// 9. Implement  JWT token for Login and Registration systems (Email/ password & social login)\. You will send the token for the Dashboard routes and verify the user\.
-
-// </br>
+9. Implement  JWT token for Login and Registration systems (Email/ password & social login)\. You will send the token for the Dashboard routes and verify the user\.
 
 // 10. Create a 404 page\. Add any interesting image/ animation & a back-to-home button on the 404 page\. **Do not add header & footer in this page**\.
-
-// </br>
-
-// <hr/>
-// <br/>
 
 // ## :writing_hand: **Optional Task:**
 
@@ -262,10 +213,9 @@ Show all approved classes on this page. Each Class will have the following:
 
 // 4. If you want, you can add an intro video button in each Class information; clicking on this button will open a modal that will play the video\.
 
-// 5. Add a button on the Instructor called see classes. After clicking on an instructor's **See Classes** button, a student can see all the Classes taken by that Instructor. Only classes **approved by the admin**\ will be displayed here. If any classes of the Instructor are in the **pending/ denied** status in the Instructor's dashboard, then they will not be shown here\. **See point 12(3)**\. 
+// 5. Add a button on the Instructor called see classes. After clicking on an instructor's **See Classes** button, a student can see all the Classes taken by that Instructor. Only classes **approved by the admin**\ will be displayed here. If any classes of the Instructor are in the **pending/ denied** status in the Instructor's dashboard, then they will not be shown here\. **See point 12(3)**\.
 
 // 6. **Following Feature** A student will see all instructors he/she is following after clicking the **Follow button** on the **Instructors page**. To implement this feature, you must add a follow button on the instructors page. Also, after clicking on the **Follow button**, show an **Unfollow button**.On clicking the **Unfollow button**, a student can unfollow an instructor, and the Instructor will disappear from the following page. Make sure to make the **Follow button active** on the **Instructors Page** again.
-
 
 // 7. (optional) **Profile page will be private:**
 
@@ -282,13 +232,8 @@ Show all approved classes on this page. Each Class will have the following:
 //         **Update Action:** a user can update their information **except role and email**on clicking the Update button.
 
 // 8. - (optional) **Updated button Action:** An instructor can update a Class information. You can show the form either in a modal or in another route. The **update button** will be **disabled** if the status is approved.
-// <br/>
 
-
-// 9. - **Manage Students:** On the Manage Classes, there will be a button called show students. Clicking on that link will take an admin to a route where the admin will see students for a particular class. An admin will be able to kick out a student from a class. 
-
-// <hr/>
-// <br/>
+// 9. - **Manage Students:** On the Manage Classes, there will be a button called show students. Clicking on that link will take an admin to a route where the admin will see students for a particular class. An admin will be able to kick out a student from a class.
 
 // ## :pushpin: **What to submit:**
 
@@ -296,9 +241,6 @@ Show all approved classes on this page. Each Class will have the following:
 // - Your server-side code GitHub repository
 // - Your live website link
 // - Admin email and password.
-
-// <hr/>
-// <br/>
 
 // ## :page_with_curl: **Some Guidelines:**
 
@@ -309,8 +251,10 @@ Show all approved classes on this page. Each Class will have the following:
 // 5. Be strategic about the electricity issue.
 // 6. Use ChatGPT to generate JSON data. You can use chatGPT for other purposes as well.
 
-// <hr/>
-// <br/>
-
 // ### Earn your Reward:
 // The greatest joys of life aren't gifted; they're earned.
+
+// button
+<button className="btn text-lg text-white bg-[#04AA6D] hover:bg-[#04AA6D] capitalize">
+  back to home
+</button>;
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import brandLogo from '../../../assets/images/logo/logo-default.png';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="bg-gray-700 text-white">
+      <div className="container mx-auto py-8 pl-5">
+        <div className="flex flex-wrap">
+          <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-4 sm:mb-0">
+            <h4 className="text-lg font-bold">Future Force Cricket Academy</h4>
+            <img src={brandLogo} alt="" />
+            <p className="mt-2 text-sm">
+              Train with the best, become the best!
+            </p>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-4 sm:mb-0">
+            <h4 className="text-lg font-bold">Contact</h4>
+            <p className="mt-2 text-sm">Email: contact@example.com</p>
+            <p className="mt-1 text-sm">Phone: 123-456-7890</p>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 mb-4 sm:mb-0">
+            <h4 className="text-lg font-bold">Address</h4>
+            <p className="mt-2 text-sm">123 Street Name</p>
+            <p className="mt-1 text-sm">City, State, Zip Code</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-900 text-center py-4">
+        <div className="container mx-auto">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Your Website. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
