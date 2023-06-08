391f0be54c7fb79849cd6106350b52881e37ce34
Bajlar01~ <bajlarrahman24@gmail.com>
2023-06-08 10:57:38 +0600

    error page done

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import error from '../../assets/images/error/404.gif';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <img src={error} alt="" />
+      <div className="text-center -mt-5">
+        <Link to="/">
+          <button className="btn text-lg text-white bg-[#04AA6D] hover:bg-[#04AA6D] capitalize">
+            back to home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Error from "../pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Error></Error>,
+  },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
