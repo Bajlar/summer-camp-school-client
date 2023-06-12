f5ab38c32e0a47423b1f869bbdd151a24d767124
Bajlar01~ <bajlarrahman24@gmail.com>
2023-06-12 16:26:50 +0600

    single instructor show the display

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import SingleInstructor from './SingleInstructor';
 
 const Instructors = () => {
   const [instructors, setInstructors] = useState([]);
@@ -14,12 +15,12 @@ const Instructors = () => {
   return (
     <div className="max-w-6xl mx-auto my-14 pt-12">
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {
-          instructors.length
-        }
-        {/* {classes.map((classes) => (
-          <SingleClass key={classes._id} classes={classes}></SingleClass>
-        ))} */}
+        {instructors.map((instructor) => (
+          <SingleInstructor
+            key={instructor._id}
+            instructor={instructor}
+          ></SingleInstructor>
+        ))}
       </div>
     </div>
   );
