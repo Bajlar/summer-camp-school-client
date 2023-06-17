af09db3ac49526789b1d23a51e243ee08893d5c8
Bajlar01~ <bajlarrahman24@gmail.com>
2023-06-17 23:47:55 +0600

    manage class button impliment successfull

--- src/pages/Dashboard/Feedback/Feedback.jsx
+++ src/pages/Dashboard/Feedback/Feedback.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const Feedback = () => {
+  const { state } = useLocation();
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const res = await axiosSecure.patch(`/classes/admin/feedback/${state}`, {
+      feedback: data.feedback,
+    });
+    console.log( data);
+    console.log(res.data);
+    if (res.data?.modifiedCount) {
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Feedback Done Successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      reset();
+    }
+  };
+
   return (
-    <div className="w-full min-h-screen p-4">
-      <form>
+    <div className="min-h-screen p-4">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="md:w-full">
           <textarea
             className="pt-5 text-gray"
-            name=""
+            {...register("feedback", { required: true })}
+            name="feedback"
             id=""
             placeholder="Text your Feedback"
             cols="80"
             rows="5"
           ></textarea>
+          <br />
+          {errors.feedback && (
+            <span className="text-red-600">Feedback is required</span>
+          )}
         </div>
         <div className=" mt-4">
           <input className="btn btn-sm capitalize" type="submit" value="Send" />
--- src/pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,11 +1,105 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import useClasses from "../../../hooks/useClasses";
-import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ManageClasses = () => {
-  const [classes] = useClasses();
-  // console.log(classes);
+  const navigate = useNavigate();
+  const { data: classes = [], refetch } = useQuery(["classes"], async () => {
+    const res = await fetch(
+      "https://summer-camp-school-server-bajlar.vercel.app/classes"
+    );
+    return res.json();
+  });
+
+  const pendingStatus = classes.filter((item) => item.status === "pending");
+  const approvedStatus = classes.filter((item) => item.status === "approved");
+  const deniedStatus = classes.filter((item) => item.status === "denied");
+
+  const handleMakePending = (item) => {
+    fetch(
+      `https://summer-camp-school-server-bajlar.vercel.app/classes/admin/status/${item._id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: "pending" }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${item.className} is Pending Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  const handleMakeApproved = (item) => {
+    fetch(
+      `https://summer-camp-school-server-bajlar.vercel.app/classes/admin/status/${item._id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: "approved" }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${item.className} is Approved Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+        }
+      });
+  };
+
+  const handleMakeDenied = (item) => {
+    fetch(
+      `https://summer-camp-school-server-bajlar.vercel.app/classes/admin/status/${item._id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: "denied" }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${item.className} is Denied Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+          navigate("/dashboard/feedback", {
+            state: item._id,
+          });
+        }
+      });
+  };
 
   return (
     <div className="w-full min-h-screen">
@@ -21,7 +115,6 @@ const ManageClasses = () => {
           {/* head */}
           <thead>
             <tr>
-              <th className="font-semibold text-md"></th>
               <th className="font-semibold text-md">Class Image</th>
               <th className="font-semibold text-md">Class Name</th>
               <th className="font-semibold text-md">Instructor Name</th>
@@ -34,9 +127,8 @@ const ManageClasses = () => {
             </tr>
           </thead>
           <tbody>
-            {classes.map((item, index) => (
+            {pendingStatus.map((item) => (
               <tr key={item._id}>
-                <th>{index + 1}</th>
                 <td>
                   <div className="flex items-center space-x-3">
                     <div className="avatar">
@@ -60,15 +152,200 @@ const ManageClasses = () => {
                 </td>
                 <td className="text-orange-500">${item.price}</td>
                 <td>
-                  <button className="btn btn-sm capitalize">Pending</button>
+                  <div className="flex gap-6">
+                    {item.status === "pending" ? (
+                      <span className="text-[#f08e00] font-inter font-medium">
+                        Pending
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakePending(item)}
+                        className="btn btn-sm text-white bg-[#D1A054] hover:bg-[#eca841]"
+                      >
+                        Pending
+                      </button>
+                    )}
+                  </div>
                 </td>
                 <td>
-                  <button className="btn btn-sm capitalize">Approved</button>
+                  <div className="flex gap-6">
+                    {item.status === "approved" ? (
+                      <span className="text-blue-700 font-inter font-medium">
+                        Approved
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeApproved(item)}
+                        className="btn btn-sm text-white bg-[#04AA6D] hover:bg-[#10ce88]"
+                      >
+                        Approved
+                      </button>
+                    )}
+                  </div>
                 </td>
                 <td>
-                  <Link to="/dashboard/feedback">
-                    <button className="btn btn-sm capitalize">Denied</button>
-                  </Link>
+                  <div className="flex gap-6">
+                    {item.status === "denied" ? (
+                      <span className="text-red-600 font-inter font-medium">
+                        Denied
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeDenied(item)}
+                        className="btn btn-sm text-white bg-red-500 hover:bg-red-700"
+                      >
+                        Denied
+                      </button>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+            {approvedStatus.map((item) => (
+              <tr key={item._id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="image w-20 h-14">
+                        <img src={item.image} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.className}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.instructorName}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.instructorEmail}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.availableSeats}</div>
+                </td>
+                <td className="text-orange-500">${item.price}</td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "pending" ? (
+                      <span className="text-[#f08e00] font-inter font-medium">
+                        Pending
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakePending(item)}
+                        className="btn btn-sm text-white bg-[#D1A054] hover:bg-[#eca841]"
+                      >
+                        Pending
+                      </button>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "approved" ? (
+                      <span className="text-blue-700 font-inter font-medium">
+                        Approved
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeApproved(item)}
+                        className="btn btn-sm text-white bg-[#04AA6D] hover:bg-[#10ce88]"
+                      >
+                        Approved
+                      </button>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "denied" ? (
+                      <span className="text-red-600 font-inter font-medium">
+                        Denied
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeDenied(item)}
+                        className="btn btn-sm text-white bg-red-500 hover:bg-red-700"
+                      >
+                        Denied
+                      </button>
+                    )}
+                  </div>
+                </td>
+              </tr>
+            ))}
+            {deniedStatus.map((item) => (
+              <tr key={item._id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="image w-20 h-14">
+                        <img src={item.image} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.className}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.instructorName}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.instructorEmail}</div>
+                </td>
+                <td>
+                  <div className="text-[#737373]">{item.availableSeats}</div>
+                </td>
+                <td className="text-orange-500">${item.price}</td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "pending" ? (
+                      <span className="text-[#f08e00] font-inter font-medium">
+                        Pending
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakePending(item)}
+                        className="btn btn-sm text-white bg-[#D1A054] hover:bg-[#eca841]"
+                      >
+                        Pending
+                      </button>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "approved" ? (
+                      <span className="text-blue-700 font-inter font-medium">
+                        Approved
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeApproved(item)}
+                        className="btn btn-sm text-white bg-[#04AA6D] hover:bg-[#10ce88]"
+                      >
+                        Approved
+                      </button>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <div className="flex gap-6">
+                    {item.status === "denied" ? (
+                      <span className="text-red-600 font-inter font-medium">
+                        Denied
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleMakeDenied(item)}
+                        className="btn btn-sm text-white bg-red-500 hover:bg-red-700"
+                      >
+                        Denied
+                      </button>
+                    )}
+                  </div>
                 </td>
               </tr>
             ))}
