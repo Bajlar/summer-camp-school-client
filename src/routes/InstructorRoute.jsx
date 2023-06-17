a0bf168fc56487ed68f0c2bee427fd65a80bb2d0
Bajlar01~ <bajlarrahman24@gmail.com>
2023-06-17 17:28:05 +0600

    code cliner

--- src/routes/InstructorRoute.jsx
+++ src/routes/InstructorRoute.jsx
@@ -20,29 +20,3 @@ const InstructorRoute = ({ children }) => {
 };
 
 export default InstructorRoute;
-
-
-
-
-
-// import { Navigate, useLocation } from "react-router-dom";
-
-// import Loader from "../components/Loader";
-// import useAuth from "../hooks/useAuth";
-// import useInstructor from "../hooks/useInstructor";
-
-// const InstructorRoute = ({ children }) => {
-//   const { user, loading } = useAuth();
-//   const [isInstructor, isInstructorLoading] = useInstructor();
-//   const location = useLocation();
-
-//   if (loading || isInstructorLoading) {
-//     return <Loader />;
-//   }
-//   if (user && isInstructor) {
-//     return children;
-//   }
-//   return <Navigate to="/" state={{ from: location }} replace />;
-// };
-
-// export default InstructorRoute;
